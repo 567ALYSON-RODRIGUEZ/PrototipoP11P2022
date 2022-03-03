ae0ea2c792c8234275a9b44aeaa426cddac28594
Alyson Rodríguez <arodriguezq3@miumg.edu.gt>
2022-03-03 13:36:31 -0600

    if

--- 9959-21-829 AlysonRodriguez.cpp
+++ 9959-21-829 AlysonRodriguez.cpp
@@ -6,3 +6,15 @@ int main(){
     int N=10;
     int v [N] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
     int x = 1;
+
+     while (x<N){
+        x++;
+        x = x * x;
+ }
+    for(x=N-1; x >= 0; x--);
+    {
+        if (v[x] == 0){
+            v[x] = v[x-1] + v[x+1];
+            cout<< printf("%d , ", v[x]);
+                      }
+
